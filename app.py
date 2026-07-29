43bef60a799fa2d2f9745ed7d4e009b4e2956541
bradnielsen2981 <brad_nielsen29@hotmail.com>
2026-07-29 12:05:29 +1000

    commit

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
 logging.basicConfig(filename='logs/flask.log', level=logging.INFO)
 sys.tracebacklimit = 10
-DATABASE = Database('test.db', log=app.logger)
+DATABASE = Database('database/test.db', log=app.logger)
 
-
-
+@app.route('/backdoor')
+def backdoor():
+    results = DATABASE.ViewQuery('SELECT * FROM users')
+    return jsonify(results)
 
 #---VIEW FUNCTIONS----------------------------------------------------
 @app.route('/', methods=['GET', 'POST'])
@@ -42,6 +44,12 @@ def register():
         firstname = request.form.get('firstname')
         lastname = request.form.get('lastname')
         confirm_password = request.form.get('confirm_password')
+        if password != confirm_password:
+            message = "Passwords do not match. Please try again."
+            return render_template('register.html', message=message)
+        else:
+            DATABASE.ModifyQuery('INSERT INTO users (email, password, firstname, lastname) VALUES (?, ?, ?, ?)', (email, password, firstname, lastname))
+            return redirect('/')
 
     app.logger.info("Register")
     return render_template('register.html')
